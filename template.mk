16324e35969ae51bc634f018cdf22bdc8df70739
turbshas <turbshas@gmail.com>
2025-11-23 23:26:16 -0500

    Squashed commit of the following:
    commit b0a759650829d1a8435bf921c9c13cfab8da194b
    Author: turbshas <turbshas@gmail.com>
    Date:   Sun Nov 23 23:25:41 2025 -0500
    
        Makefile fiddling
    
        Move all functionality into template.mk
        Use relative paths

--- template.mk
+++ template.mk
@@ -1,4 +1,4 @@
-# Requires MAKEFILE_DIR and MAIN_MAKEFILE_DIR variables to be set
+# Requires MAIN_MAKEFILE_DIR variables to be set
 ifeq ($(MAKELEVEL),0)
 
 ifeq ($(MAKECMDGOALS),)
@@ -10,7 +10,17 @@ else
 endif
 
 else
-SRC_FILES += $(patsubst $(CURDIR)/%,%,$(wildcard $(MAKEFILE_DIR)/*.$(SRC_FILE_EXTENSION)))
+NUM_MAKEFILES = $(words $(MAKEFILE_LIST))
+# This file is the last in the makefile list, the one we want is second-last.
+MAKEFILE_PATH := $(word $(NUM_MAKEFILES), PushItem $(MAKEFILE_LIST))
+# Strip the trailing / from the path.
+MAKEFILE_DIR := $(patsubst %/,%, $(dir $(MAKEFILE_PATH)))
+
+SEEN_FILES := $(patsubst $(CURDIR)/%,%,$(wildcard $(MAKEFILE_DIR)/*.$(SRC_FILE_EXTENSION)))
+SRC_FILES += $(SEEN_FILES)
 INCLUDES += -I$(MAKEFILE_DIR)
+
+SUBMODULES := $(shell ls -d $(MAKEFILE_DIR)/*/ 2>/dev/null || true)
+include $(patsubst %/, %/Makefile, $(SUBMODULES))
 endif
 
